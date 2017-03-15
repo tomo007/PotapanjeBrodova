af96752a42d93f9273a9ae634b2c1b2afbde877c
Tomica123 <tomislavblaz@gmail.com>
2017-03-15 21:18:19 +0100

    Rijesili smo polje

---
+++ PotapanjeBrodova/PotapanjeBrodova/Polje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotapanjeBrodova
+{
+   public class Polje:IEquatable<Polje>
+    {
+        public Polje(int Redak, int Stupac) {
+            this.Redak = Redak;
+            this.Stupac = Stupac;
+
+        }
+        public readonly int Redak, Stupac;
+
+        public bool Equals(Polje other)
+        {
+            return Redak == other.Redak && Stupac == other.Stupac;
+        }
+    }
+}
